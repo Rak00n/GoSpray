ce6213a785941908e8117f3fe8df39f47d493088
gortunovd <80981522+gortunovd@users.noreply.github.com>
2022-05-10 11:20:29 +0300

    Update rdpspray.go

--- rdpspray.go
+++ rdpspray.go
@@ -1,15 +1,15 @@
 package main
 
 import (
-	"github.com/Rak00n/grdp"
-	"log"
-	"os"
+	//"github.com/Rak00n/grdp"
+	//"log"
+	//"os"
 )
 
 func rdpSpray () {
-	glog.SetLevel(glog.INFO)
-	logger := log.New(os.Stdout, "", 0)
-	glog.SetLogger(logger)
-	g := rdp.NewClient(ip, glog.LEVEL(loglevel))
-	err := g.Login(domain, user, passwd)
+	//glog.SetLevel(glog.INFO)
+	//logger := log.New(os.Stdout, "", 0)
+	//glog.SetLogger(logger)
+	//g := rdp.NewClient(ip, glog.LEVEL(loglevel))
+	//err := g.Login(domain, user, passwd)
 }
